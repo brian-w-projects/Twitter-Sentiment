60133638c2c515bf53ba3c6fb2daf2f6a79cd195
brian-w-projects <brian.weinfeld@gmail.com>
2017-08-15 09:59:58 -0400

    initial commit

---
+++ app/main/views.py
@@ -0,0 +1,6 @@
+from flask import render_template, request, jsonify, session, flash, redirect, url_for, current_app
+from . import main
+# from .forms import
+# from ..models import
+from .. import db
+
